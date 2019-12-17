ee63e795e66c48e0b86407721550e345debbfff4
wusiyu <stephenwu2019@163.com>
2019-12-17 11:46:14 +0800

    merge generate

--- cmd/hdwallet.go
+++ cmd/hdwallet.go
@@ -1,7 +1,15 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
+
+	hdwallet "github.com/miguelmota/go-ethereum-hdwallet"
 	"github.com/spf13/cobra"
+)
+
+var (
+	bits int
 )
 
 var hdwalletCmd = &cobra.Command{
@@ -12,6 +20,21 @@ var hdwalletCmd = &cobra.Command{
 	},
 }
 
+var hdgenerateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "generate mnemonic",
+	Run: func(cmd *cobra.Command, args []string) {
+		mnemonic, err := hdwallet.NewMnemonic(bits)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(mnemonic)
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(hdwalletCmd)
+
+	hdwalletCmd.AddCommand(hdgenerateCmd)
+	hdgenerateCmd.Flags().IntVarP(&bits, "bits", "b", 256, "set mnemonic's length")
 }
