01c094081088c2d48b0ea90af7607acd9cf72e00
wusiyu <stephenwu2019@163.com>
2019-12-18 14:40:58 +0800

    add url

--- conf/conf.go
+++ conf/conf.go
@@ -1,5 +1,7 @@
 package conf
 
-var ShhUrl string = "ws://localhost:8546"
+//var ShhUrl string = "ws://localhost:8546"
 
 //var ShhUrl string = "ws://192.168.0.111:8546"
+
+var ShhUrl string = "wss://mainnet.infura.io/ws/v3/9f28b75fa35c4dc2ae401196993494f5"
