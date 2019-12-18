664303c46be8807efe04fa2efa6fc32b00b5da66
wusiyu <stephenwu2019@163.com>
2019-12-18 16:06:59 +0800

    send eth

--- cmd/transaction.go
+++ cmd/transaction.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+	"crypto/ecdsa"
 	"fmt"
 	"log"
 	"math/big"
@@ -9,6 +10,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/jpsiyu/ethtut-cli/conf"
 	"github.com/spf13/cobra"
@@ -17,6 +19,9 @@ import (
 var (
 	trBlock  int64
 	trTxHash string
+	trPriv   string
+	trTo     string
+	trValue  int64
 )
 
 func displayBlock(block *types.Block) {
@@ -87,6 +92,54 @@ var trTxCmd = &cobra.Command{
 	},
 }
 
+var trSendCmd = &cobra.Command{
+	Use:   "send",
+	Short: "send eth",
+	Run: func(cmd *cobra.Command, args []string) {
+		client, err := ethclient.Dial(conf.ShhUrl)
+		if err != nil {
+			log.Fatal(err)
+		}
+		privKey, err := crypto.HexToECDSA(trPriv)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pubKey := privKey.Public()
+		pubKeyECDSA, ok := pubKey.(*ecdsa.PublicKey)
+		if !ok {
+			log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+		}
+		fromAddress := crypto.PubkeyToAddress(*pubKeyECDSA)
+		nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+		if err != nil {
+			log.Fatal(err)
+		}
+		value := big.NewInt(trValue)
+		gasLimit := uint64(21000)
+		gasPrice, err := client.SuggestGasPrice(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+		toAddress := common.HexToAddress(trTo)
+		var data []byte
+		tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
+		chainID, err := client.NetworkID(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+		signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privKey)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = client.SendTransaction(context.Background(), signedTx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("tx send:", signedTx.Hash().Hex())
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(transactionCmd)
 
@@ -96,4 +149,12 @@ func init() {
 	transactionCmd.AddCommand(trTxCmd)
 	trTxCmd.Flags().StringVarP(&trTxHash, "hash", "x", "", "input tx hash")
 	trTxCmd.MarkFlagRequired("hash")
+
+	transactionCmd.AddCommand(trSendCmd)
+	trSendCmd.Flags().StringVarP(&trPriv, "priv", "p", "", "private key")
+	trSendCmd.Flags().StringVarP(&trTo, "to", "t", "", "to address")
+	trSendCmd.Flags().Int64VarP(&trValue, "value", "v", 0, "value in wei")
+	trSendCmd.MarkFlagRequired("priv")
+	trSendCmd.MarkFlagRequired("to")
+	trSendCmd.MarkFlagRequired("value")
 }
