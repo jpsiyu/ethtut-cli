da00b36b12245b2f36daf47c955e981a97ca6bce
wusiyu <stephenwu2019@163.com>
2019-12-18 14:58:31 +0800

    add conf

--- conf/conf.go
+++ conf/conf.go
@@ -4,4 +4,6 @@ package conf
 
 //var ShhUrl string = "ws://192.168.0.111:8546"
 
-var ShhUrl string = "wss://mainnet.infura.io/ws/v3/9f28b75fa35c4dc2ae401196993494f5"
+//var ShhUrl string = "wss://mainnet.infura.io/ws/v3/9f28b75fa35c4dc2ae401196993494f5"
+
+var ShhUrl string = "wss://ropsten.infura.io/ws/v3/9f28b75fa35c4dc2ae401196993494f5"
