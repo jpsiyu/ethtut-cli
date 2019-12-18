817b2cd38b78d308ed47dd19397d06b4aacd0ef5
wusiyu <stephenwu2019@163.com>
2019-12-18 16:50:48 +0800

    sign data

--- cmd/util.go
+++ cmd/util.go
@@ -2,12 +2,17 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/jpsiyu/ethtut-cli/util"
 	"github.com/spf13/cobra"
+	"log"
 )
 
 var (
 	utAmount string
+	utPriv   string
+	utData   string
 )
 
 var utilCmd = &cobra.Command{
@@ -36,6 +41,25 @@ var toEthCmd = &cobra.Command{
 	},
 }
 
+var signCmd = &cobra.Command{
+	Use:   "sign",
+	Short: "generate signature",
+	Run: func(cmd *cobra.Command, args []string) {
+		privKey, err := crypto.HexToECDSA(utPriv)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data := []byte(utData)
+		hash := crypto.Keccak256Hash(data)
+		signature, err := crypto.Sign(hash.Bytes(), privKey)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("hash:", hash.Hex())
+		fmt.Println("signature:", hexutil.Encode(signature))
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(utilCmd)
 
@@ -46,4 +70,10 @@ func init() {
 	utilCmd.AddCommand(toEthCmd)
 	toEthCmd.Flags().StringVarP(&utAmount, "amount", "a", "", "amount")
 	toEthCmd.MarkFlagRequired("amount")
+
+	utilCmd.AddCommand(signCmd)
+	signCmd.Flags().StringVarP(&utPriv, "priv", "p", "", "private key")
+	signCmd.Flags().StringVarP(&utData, "data", "d", "", "data used to sign")
+	utilCmd.MarkFlagRequired("priv")
+	utilCmd.MarkFlagRequired("data")
 }
